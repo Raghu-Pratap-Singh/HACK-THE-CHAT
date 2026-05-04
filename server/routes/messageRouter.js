c86760569db4910ab190ee16f850f378d8e7a421
Raghu-Pratap-Singh <raghupratapsinghparmar@gmail.com>
2026-05-04 23:06:47 +0530

    protections middleware update

--- server/routes/messageRouter.js
+++ server/routes/messageRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn } = require("../middlewares/isLoggedIn")
 const { get_chat, give } = require("../controllers/messageController");
-router.post("/get_chat", get_chat);
-router.post("/give_message", give);
+router.post("/get_chat", isLoggedIn, get_chat);
+router.post("/give_message", isLoggedIn, give);
 module.exports = router
