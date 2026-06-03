4bbfe52121325487eb319207f13db140a7be8bd3
Raghu-Pratap-Singh <raghupratapsinghparmar@gmail.com>
2026-06-03 20:51:55 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
 const { createServer } = require("http");
 const cookieParser = require("cookie-parser");
 const path = require("path");
@@ -16,35 +15,32 @@ const client_port = process.env.CLIENT_PORT
 
 const { initSocket } = require("./socket");
 
+const app = express();
 app.use(cors({
     origin: client_port,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
+//middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-
 // Routes
-
 app.use("/users", usersRouter);
 app.use("/messages", messageRouter);
 app.use("/LOG", levelRouter);
 app.use("/tree", treeRouter);
 
 // Create HTTP Server
-
 const server = createServer(app);
 
-
+//start socket connection
 initSocket(server);
 
-// -------------------
 // Start Server
-
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
